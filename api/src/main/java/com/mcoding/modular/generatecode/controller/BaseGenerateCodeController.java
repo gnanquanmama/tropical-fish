c695a7cb8534250d8e3ea8445bd4989345c2f51a
wuzetao <zetao_wu@163.com>
2020-06-13 22:37:16 +0800

    feature: 自动登录信息组装

--- api/src/main/java/com/mcoding/modular/generatecode/controller/BaseGenerateCodeController.java
+++ api/src/main/java/com/mcoding/modular/generatecode/controller/BaseGenerateCodeController.java
@@ -4,6 +4,7 @@ import com.mcoding.common.util.rest.ResponseResult;
 import com.mcoding.modular.generatecode.service.BaseGenerateCodeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.javasimon.aop.Monitored;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ public class BaseGenerateCodeController {
     @Resource
     private BaseGenerateCodeService baseGenerateCodeService;
 
+    @Monitored
     @ApiOperation("生成编码")
     @PostMapping("/service/generateCode/generateNextCode")
     public ResponseResult<String> generateNextCode(String targetCode) {
---
+++ core/src/main/java/com/mcoding/base/auth/LoginRequired.java
@@ -0,0 +1,16 @@
+package com.mcoding.base.auth;
+
+import java.lang.annotation.*;
+
+/**
+ * @author wzt on 2020/6/13.
+ * @version 1.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@Documented
+public @interface LoginRequired {
+
+    String sessionKey() default "currentUser";
+
+}
